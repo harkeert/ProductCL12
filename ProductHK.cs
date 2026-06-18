44e494df381e61c2d62531e6fe62222ea367f96f
Harkirat Kaur <harkiratkaur4234@conestogac.on.ca>
2026-06-18 16:35:07 -0400

    Pair12_HK initial commit for ProductCL12 project

---
+++ ProductHK.cs
@@ -0,0 +1,37 @@
+﻿namespace ProductCL12
+{
+    public class ProductHK
+    {
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public double ItemPrice { get; set; }
+        public int StockCount { get; set; }
+
+        public ProductHK(
+            int itemID,
+            string itemName,
+            double itemPrice,
+            int stockCount)
+        {
+            ItemID = itemID;
+            ItemName = itemName;
+            ItemPrice = itemPrice;
+            StockCount = stockCount;
+        }
+
+        public void IncreaseStock(int amount)
+        {
+            StockCount += amount;
+        }
+
+        public void DecreaseStock(int amount)
+        {
+            StockCount -= amount;
+        }
+
+        public int CheckStock()
+        {
+            return StockCount;
+        }
+    }
+}
